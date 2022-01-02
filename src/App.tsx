7de96459d7474bf1a3cd0e611529a2d30621aa27
caribbeanyute <smithcleon@gmail.com>
2022-01-01 21:10:40 -0500

    prelim test of video.js component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,54 @@
 import React from "react";
+import VideoJS  from "./components/VideoJS";
+
+// TODO: SETUP types for this component
 const App: React.FC = (props) => {
-  return <div>Webpack is cool!</div>;
+
+  const playerRef = React.useRef(null);
+
+  const videoJsOptions = { // lookup the options in the docs for more options
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [{
+      src: 'http://127.0.0.1:7002/live/movie.m3u8',
+      type: 'application/x-mpegURL'
+    }]
+  }
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // you can handle player events here
+    player.on('waiting', () => {
+      console.log('player is waiting');
+    });
+
+    player.on('dispose', () => {
+      console.log('player will dispose');
+    });
+  };
+
+  // const changePlayerOptions = () => {
+  //   // you can update the player through the Video.js player instance
+  //   if (!playerRef.current) {
+  //     return;
+  //   }
+  //   // [update player through instance's api]
+  //   playerRef.current.src([{src: 'http://ex.com/video.mp4', type: 'video/mp4'}]);
+  //   playerRef.current.autoplay(false);
+  // };
+
+  return (
+    <>
+      <div>Rest of app here</div>
+
+      <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+
+      <div>Rest of app here</div>
+    </>
+  );
 };
 
 export default App;
