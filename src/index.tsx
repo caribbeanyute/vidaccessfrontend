ea8eb2c634efaf97963c5da9d6cc3f760153ecb7
caribbeanyute <smithcleon@gmail.com>
2022-01-07 01:11:52 -0500

    init adding routes to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import Login from './pages/Login';
 
-render(<App />, document.body);
+// Root style
+import './index.scss';
+
+
+
+render(
+	<BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login></Login>} />
+		<Route path="/app" element={<App></App>} />
+      </Routes>
+	</BrowserRouter>
+	, document.getElementById('root'));
