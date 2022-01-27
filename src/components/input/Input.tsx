809baa9e1597a1be488922b3fe569b185f25f584
caribbeanyute <smithcleon@gmail.com>
2022-01-27 16:29:14 -0500

    single html element for input

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -16,12 +16,14 @@ const Input: FC<InputProps> = ({ label, type = 'text', onChangeText }: InputProp
 
 
   return (
+    <>
       <div className='font-face-rb text-black text-xs pt-4'>{label}</div>
       <input className="text-gray-500 py-2 px-4 mt-4 rounded-full outline outline-offset-2 outline-1 outline-grey"
         type={type}
         value={value}
         onChange={handleInputChange}  >
       </input>
+      </>
   );
 };
 
