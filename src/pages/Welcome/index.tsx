d59ed59702e154686e5a2dd433af6e013362d9e5
caribbeanyute <smithcleon@gmail.com>
2022-03-01 22:30:47 -0500

    added welcome page component

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react'
-import { connect, MapDispatchToProps, MapStateToProps } from 'react-redux';
+import { connect, MapDispatchToProps, MapStateToProps, useDispatch, useSelector } from 'react-redux';
+import { getStreams } from '../../state/actions/streamActions';
 
 import mediaplayer from './images/media_player.svg'
+
+import CardStream from 'components/card/Card.stream';
 
 
 
 
 type WelcomeProps = {
 	UI: any,
-//	WelcomeUser: typeof WelcomeUser,
+	//	WelcomeUser: typeof WelcomeUser,
 }
 
 interface StateProps {
@@ -16,22 +19,22 @@ interface StateProps {
 }
 
 const Welcome: React.FC<WelcomeProps> = ({ UI, ...props }: WelcomeProps) => {
-	const [userName, setUserName] = useState("d")
+	const dispatch = useDispatch();
+	const stream = useSelector((state) => state.stream);
 
 	useEffect(() => {
-	}, [UI])
+		dispatch(getStreams());
+	}, [dispatch])
 
-	
+
 	return (
-		<div className="container flex flex-col  h-screen w-screen">
-			<div className="grid grid-cols-2">
-				<div className='bg-white pl-20 pt-40 h-screen '>
-					<div className='font-face-rb text-black text-4xl pt-4'>Welcome Cleon</div>
-					
+		<div className="container flex flex-col">
+			<div className='font-face-rb text-black text-4xl pt-0'>Welcome Cleon</div>
+			<div className="grid grid-row-2">
+				addStream button goes here >>>>>>>>>>>>>>>>>>>>
 
-				</div>
 				<div className='flex w-full'>
-					<img className='px-20' src={mediaplayer} alt="Logo" />
+					<CardStream />
 				</div>
 			</div>
 		</div>
@@ -39,15 +42,8 @@ const Welcome: React.FC<WelcomeProps> = ({ UI, ...props }: WelcomeProps) => {
 }
 
 //this map the states to our props in this functional component
-const mapStateToProps = (state: any) => ({
-	user: state.user,
-	//UI: state.WelcomeUIReducer
-});
-//this map actions to our props in this functional component
-const mapDispatchToProps: MapDispatchToProps<DispatchProps, WelcomeProps> = {
-	//WelcomeUser
-};
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Welcome)
+
+export default Welcome
 
