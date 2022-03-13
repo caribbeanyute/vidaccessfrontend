4474c60df6339e3b4aa96c453e7993765eff626a
caribbeanyute <smithcleon@gmail.com>
2022-03-12 21:25:26 -0500

    added logic to toggle sidebar

--- src/components/mainview/MainView.tsx
+++ src/components/mainview/MainView.tsx
@@ -1,4 +1,5 @@
-import React, { FC, useState} from "react";
+import React, { FC, useState } from "react";
+import { useSelector } from "react-redux";
 import {
 	Link,
 	Outlet,
@@ -8,7 +9,7 @@ import {
 import logo from "assets/img/streamaccess-logos/streamaccess-logos-transparent.png";
 
 import { Home, Video, Music, Settings, Smile } from "react-feather";
-import './sidebar.scss'
+import './MainView.scss'
 
 import Header from '../header/Header';
 
@@ -24,15 +25,15 @@ const navItems = [
 	{
 		label: "Video Sources",
 		icon: () => <Video className="" size={iconSize} color="white" />,
-		path:  "/app",
+		path: "/app",
 		link: <Link to="/app">Video</Link>
-	},
+	}]/*,
 	{
 		label: "Audio Sources",
 		icon: () => <Music className="" size={iconSize} color="white" />,
 		path: "",
 		link: <Link to="/app">Audio</Link>
-	}, 
+	},
 	{
 		label: "Manage Source",
 		icon: () => <Smile className="" size={iconSize} color="white" />,
@@ -47,7 +48,7 @@ const navItems = [
 	},
 
 
-];
+];*/
 
 
 
@@ -63,7 +64,7 @@ const NavButton: FC<NavButtonProps> = ({ label, path, icon, link }: NavButtonPro
 	const { pathname } = useLocation();
 	console.log(pathname);
 	return (
-		<button className={`navButton  ${(pathname === path) ? "bg-slate-700" : "bg-blackBg" } hover:bg-darkslategray rounded-lg
+		<button className={`navButton  ${(pathname === path) ? "bg-slate-700" : "bg-blackBg"} hover:bg-darkslategray rounded-lg
 		hover:shadow flex flex-row items-center content-center px-2 mx-2 py-2 m-2`}>
 			<div className="pr-1">
 				{icon()}
@@ -73,50 +74,50 @@ const NavButton: FC<NavButtonProps> = ({ label, path, icon, link }: NavButtonPro
 	)
 }
 
-export default function SidebarExample() {
-	const [shown, setShown] = useState(false);
+export default function MainView() {
+	const { sideBarVisibility } = useSelector((state: RootState) => state.ui);
 
-	return (<>
-		<Header />
-		<div className="flex flex-no-wrap rounded-full">
-			
+	return (
+		<div className="flex flex-col h-screen">
+			<Header className="w-full" />
+
 			{/* Sidebar starts */}
-			{/* Remove class [ hidden ] and replace [ sm:flex ] with [ flex ] */}
-			<div style={{ "minHeight": '100vh' }} className="w-64 absolute sm:relative bg-blackBg shadow md:h-full flex-col justifybetween hidden sm:flex">
-				<div className="logo mt-5">
-					<img style={{
-						"objectFit": "contain"
-					}} src={logo} alt="Logo" />
-					
+			
+			<div className="flex flex-grow rounded-full">
+			{ sideBarVisibility && (
+				<div style={{}} className="w-64 absolute sm:relative bg-blackBg shadow h-full flex-col justifybetween hidden sm:flex">
+					<div className="logo mt-5">
+						<img style={{
+							"objectFit": "contain"
+						}} src={logo} alt="Logo" />
+
+					</div>
+
+
+					<nav className="navItems flex flex-col">
+						{
+							navItems.map((navItem, indx) => (
+								<NavButton key={indx} label={navItem.label} icon={navItem.icon} path={navItem.path} link={navItem.link} />
+							))
+						}
+
+					</nav>
+
+					<div className="mb-auto">
+						{ }
+					</div>
+
+				</div>)
+				}
+
+				<div className="container mx-auto">
+					{/* Remove className [ border-dashed border-2 border-gray-300 ] to remove dotted border */}
+
+						<Outlet />
+	
+
 				</div>
-
-
-				<nav className="navItems flex flex-col">
-					{
-						navItems.map((navItem, indx) => (
-							<NavButton key={indx} label={navItem.label} icon={navItem.icon} path={navItem.path} link={navItem.link}>
-							</NavButton>
-						))
-					}
-
-				</nav>
-
-				<div className="mb-auto">
-					{}
-				</div>
-
-			</div>
-
-           {/* className="container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6" */} 
-		   
-			<div className="container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6">
-				{/* Remove className [ border-dashed border-2 border-gray-300 ] to remove dotted border */}
-				<div className="rounded border-dashed border-2 border-gray-300">
-					<Outlet />
-				</div>
-
 			</div>
 		</div>
-		</>
 	);
 }
