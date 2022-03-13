32157063ba03a7b3ae2be6179981e78fb0acb714
caribbeanyute <smithcleon@gmail.com>
2022-03-12 21:23:38 -0500

    stripped app component and added customrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,13 @@
 import React from "react";
-import VideoJS  from "./components/video/VideoJS";
-import { useDispatch, useSelector} from 'react-redux'
-import RootState from "state/reducers/index";
-
-const Index = () => {
-	return <div className="bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden">
-	<div className="sm:flex sm:items-center px-6 py-4">
-	  <img className="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0" src="https://avatars2.githubusercontent.com/u/4323180?s=400&u=4962a4441fae9fba5f0f86456c6c506a21ffca4f&v=4" alt=""/>
-	  <div className="text-center sm:text-left sm:flex-grow">
-		<div className="mb-4">
-		  <p className="text-xl leading-tight">Adam Wathan</p>
-		  <p className="text-sm leading-tight text-grey-dark">Developer at NothingWorks Inc.</p>
-		</div>
-		<div>
-		  <button className="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple hover:bg-purple hover:text-white">Message</button>
-		</div>
-	  </div>
-	</div>
-  </div>;
-  };
 
 // TODO: SETUP types for this component
 // TODO: Setup unit tests
 const App: React.FC = (props) => {
-  const dispatch = useDispatch();
-	const videoJSConfig = useSelector((state: RootState) => state.videoJS);
+  
 
-  const playerRef = React.useRef(null);
-
-  const videoJsOptions = { // lookup the options in the docs for more options
-    autoplay: true,
-    controls: true,
-    responsive: true,
-    fluid: true,
-    sources: [{
-      src: 'https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8',
-      type: 'application/x-mpegURL'
-    }]
-  }
-
-  const handlePlayerReady = (player) => {
-    playerRef.current = player;
-
-    // you can handle player events here
-    player.on('waiting', () => {
-      console.log('player is waiting');
-    });
-
-    player.on('dispose', () => {
-      console.log('player will dispose');
-    });
-  };
-
-  // const changePlayerOptions = () => {
-  //   // you can update the player through the Video.js player instance
-  //   if (!playerRef.current) {
-  //     return;
-  //   }
-  //   // [update player through instance's api]
-  //   playerRef.current.src([{src: 'http://ex.com/video.mp4', type: 'video/mp4'}]);
-  //   playerRef.current.autoplay(false);
-  // };
 
   return (
     <>
-      <VideoJS options={videoJSConfig} onReady={handlePlayerReady} />
-
-      <div>Rest of app here</div>
     </>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,43 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from 'react-redux';
 import App from "./App";
+import VideoPage from "./pages/Video";
 import Login from './pages/Login';
 import Welcome from "./pages/Welcome";
-import Sidebar from "./components/sidebar/sidebar";
+import Sidebar from "./components/mainview/MainView";
 import axios from "axios";
-
-
-axios.defaults.baseURL = "/api";
 
 
 // Root style
 import './index.scss';
-
+//Redux
 import { store } from './state';
+//react-router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CustomRouter } from "./utils/CustomRouter";
+import customHistory from "./utils/history";
 
-
+axios.defaults.baseURL = "/api";
 
 const Index = () => (
 
 	<Provider store={store}>
-		<BrowserRouter>
+		<CustomRouter history={customHistory}>
 			<Routes>
 				<Route path="/login" element={<Login />} />
 				{//<Route path="/app" element={<App/>} />
 				}
 				<Route path="/" element={<Sidebar />} >
 					<Route index={true} element={<Welcome />} />
-					<Route path="/app" element={<App/>}>
+					<Route path="/app" element={<VideoPage />}>
 
 					</Route>
 
 				</Route>
 				<Route path="*" element={"Not Found"}></Route>
 			</Routes>
-		</BrowserRouter>
+		</CustomRouter>
 	</Provider>
 );
 
