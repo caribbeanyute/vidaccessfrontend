28ac7a6271aac991d8eef37d43952b20a1ee81c8
caribbeanyute <smithcleon@gmail.com>
2022-03-12 21:26:52 -0500

    added welcome page

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -28,9 +28,9 @@ const Welcome: React.FC<WelcomeProps> = ({ UI, ...props }: WelcomeProps) => {
 
 
 	return (
-		<div className="container flex flex-col">
+		<div className="flex flex-col mt-10 px-6 h-5/6 rounded border-dashed border-2 border-gray-300">
 			<div className='flex flex-row justify-items-center mt-2'>
-				<div className='font-face-rb text-black md:text-1 text-4xl pt-0 place-self-start'>My Streams</div>
+				<div className='font-face-rb text-black text-xl md:text-2xl place-self-start'>Streams</div>
 				<AddButton className="ml-auto" label='Create Stream' ></AddButton>
 			</div>
 			<div className="pt-5 grid grid-cols-1 md:grid-cols-3 gap-2 justify-items-center">
