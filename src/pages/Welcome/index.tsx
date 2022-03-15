a03e8f78e95a12224e85dced114fbd967109a25c
caribbeanyute <smithcleon@gmail.com>
2022-03-15 16:57:47 -0500

    added routes constant

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -8,6 +8,8 @@ import AddButton from 'components/button/AddButton';
 
 import RootState from "state/reducers/index"
 import { getStreams } from '../../state/actions/streamActions';
+import { Link } from 'react-router-dom';
+import routes from '../../utils/routes';
 
 
 type WelcomeProps = {
@@ -31,13 +33,17 @@ const Welcome: React.FC<WelcomeProps> = ({ UI, ...props }: WelcomeProps) => {
 		<div className="flex flex-col mt-10 px-6 h-5/6 rounded border-dashed border-2 border-gray-300">
 			<div className='flex flex-row justify-items-center mt-2'>
 				<div className='font-face-rb text-black text-xl md:text-2xl place-self-start'>Streams</div>
-				<AddButton className="ml-auto" label='Create Stream' ></AddButton>
+				<Link to={routes.addStream} className='ml-auto'>
+				<AddButton className="" label='Create Stream' >
+					</AddButton>
+				</Link>
 			</div>
 			<div className="pt-5 grid grid-cols-1 md:grid-cols-3 gap-2 justify-items-center">
 
+
 				{
-					stream.streams.map((stream, index) =>(
-						
+					stream.streams.map((stream, index) => (
+
 						<CardStream className="user pt-3" key={index} isLive={stream.live} stream={stream} />
 					))
 				}
---
+++ src/utils/routes.ts
@@ -0,0 +1,6 @@
+export default {
+	home: '/',
+	login: '/login',
+	video: '/video',
+	addStream: '/add-stream',
+}
