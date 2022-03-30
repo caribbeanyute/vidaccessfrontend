f1822a43c84806bd9ca04a46bcbaba9c1cd1831e
caribbeanyute <smithcleon@gmail.com>
2022-03-30 14:02:44 -0500

    updated add button props

--- src/components/button/AddButton.tsx
+++ src/components/button/AddButton.tsx
@@ -1,29 +1,24 @@
-import React, { ReactChild, ReactChildren } from 'react';
+import React, {ReactChild, ReactChildren } from 'react';
 
-import { PlusCircle } from 'react-feather';
+import {PlusCircle} from 'react-feather';
 
 type AddButtonProps = {
-	className: string,
-	href: string,
-	isActive: boolean,
-	label:string,
-	type?: string,
-	children: ReactChild | ReactChildren
-	onClick: (e: React.MouseEvent<HTMLElement>) => void
-
+    className: string,
+    href: string,
+    isActive?: boolean,
+    label: string,
+    type?: string,
+    children: ReactChild | ReactChildren
 }
 
-
-export default function AddButton({className, href, isActive, children, label , onClick }: AddButtonProps) {
-	return (
-		<button disabled type="button" className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4
+export default function AddButton({className, label}: AddButtonProps) {
+    return (
+        <button disabled type="button" className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4
 		 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2
-		  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center `+className}
-		  onClick={(event) => onClick(event)}
-		  
-		  >
-			<PlusCircle className='inline mr-3 w-4 h-4 text-white'></PlusCircle>
-			{label}
-		</button>
-	)
+		  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center ` + className}
+        >
+            <PlusCircle className='inline mr-3 w-4 h-4 text-white'></PlusCircle>
+            {label}
+        </button>
+    )
 }
