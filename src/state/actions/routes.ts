2cff876498a19c454e0e75850c08814c232b7a03
caribbeanyute <smithcleon@gmail.com>
2022-05-05 21:26:45 -0500

    added logic to dispatch routes

---
+++ src/state/actions/routes.ts
@@ -0,0 +1,9 @@
+import customHistory  from '../../utils/history';
+
+// Route for components
+// export const notifications = () => dispatch => customHistory.push('/notifications');
+//export const uploadBook = () => dispatch => customHistory.push('/upload-book');
+//export const allBooks = () => dispatch => customHistory.push('/home');
+export const home = () => dispatch => customHistory.push('/home');
+//export const history = () => dispatch => customHistory.push('/history');
+//export const settings = () => dispatch => customHistory.push('/settings')
