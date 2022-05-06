d838a710fdfe183b89b61100c660642f1f0390f1
caribbeanyute <smithcleon@gmail.com>
2022-05-05 21:25:48 -0500

    split sidebar into separate component

--- src/components/mainview/MainView.tsx
+++ src/components/mainview/MainView.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
 	Link,
@@ -6,113 +6,35 @@ import {
 	useLocation
 } from "react-router-dom";
 
-import logo from "assets/img/streamaccess-logos/streamaccess-logos-transparent.png";
-import { Home, Video, LogOut } from "react-feather";
 import './MainView.scss'
-import routes from "../../utils/routes";
 import Header from '../header/Header';
-import { logoutUser } from '../../state/actions/authActions'
+import Sidebar from "../sidebar";
 
-const iconSize = 19;
-
-type LogoutProps = {
-	className?: string,
-	size: number,
-	color: string,
-}
-
-const LogOutFC: FC<LogoutProps> = ({ className, size, color }: LogoutProps) => {
-	return (
-		<LogOut className={className} size={iconSize} color={color} />
-	)
-}
-
-
-
-type NavButtonProps = {
-	label: string,
-	path: string,
-	icon: () => React.ReactNode,
-	onClick?: () => void,
-}
-
-const NavButton: FC<NavButtonProps> = ({ label, path, icon, onClick }: NavButtonProps) => {
-	const { pathname } = useLocation();
-
-	return (
-		<Link to={path !== "/logout" ? path : "#"} className={`navButton  ${(pathname === path) ? "bg-slate-700" : "bg-blackBg"} hover:bg-darkslategray rounded-lg
-		hover:shadow flex flex-row items-center content-center px-2 mx-2 py-2 m-2`}
-			onClick={onClick}>
-			<div className="pr-1">
-				{icon()}
-			</div>
-			<div className="navButtonText text-white text-center text-xs pl-4 ">{label}</div>
-		</Link>
-	)
-}
 
 
 export default function MainView() {
 	const { sideBarVisibility } = useSelector((state: RootState) => state.ui);
 	const dispatch = useDispatch();
 
-	const navItems = [
-		{
-			label: 'Home',
-			icon: () => <Home className="" size={iconSize} color="white" />,
-			path: "/",
 
-		},
-		{
-			label: "Video",
-			icon: () => <Video className="" size={iconSize} color="white" />,
-			path: routes.video,
-
-		},
-		{
-			label: "Logout",
-			icon: () => <LogOutFC className="" size={iconSize} onClick color="white" />,
-			path: "/logout",
-			onClick: () => dispatch(logoutUser())
-		}];
 
 	return (
 		<div className="flex flex-col h-screen">
 			<Header className="w-full" />
 
-			{/* Sidebar starts */}
 
-			<div className="flex flex-grow rounded-full">
+
+			<div className="flex items-center flex-grow">
 				{sideBarVisibility && (
-					<div style={{}} className="w-64 z-20 absolute md:relative bg-blackBg shadow h-full flex-col justify-between sm:flex">
-						<div className="logo mt-5">
-							<img style={{
-								"objectFit": "contain"
-							}} src={logo} alt="Logo" />
-
-						</div>
-
-
-						<nav className="navItems flex flex-col">
-							{
-								navItems.map((navItem, indx) => (
-									<NavButton key={indx} label={navItem.label} icon={navItem.icon} path={navItem.path} onClick={navItem.onClick} />
-								))
-							}
-
-						</nav>
-
-						<div className="mb-auto">
-							{ }
-						</div>
-
-					</div>)
+					<Sidebar />
+				)
 				}
-
-				<div className="container mx-auto">
-					<Outlet />
-				</div>
+				<Outlet />
 			</div>
+
+
+
+
 		</div>
 	);
 }
---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,107 @@
+import React, { FC, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+	Link,
+	NavLink,
+	useLocation
+} from "react-router-dom";
+
+import logo from "assets/img/streamaccess-logos/streamaccess-logos-transparent.png";
+import { Home, Video, LogOut } from "react-feather";
+import routes from "../../utils/routes";
+import { logoutUser } from '../../state/actions/authActions'
+
+
+const iconSize = 19;
+type LogoutProps = {
+	className?: string,
+	size: number,
+	color: string,
+}
+
+const LogOutFC: FC<LogoutProps> = ({ className, size, color }: LogoutProps) => {
+	return (
+		<LogOut className={className} size={iconSize} color={color} />
+	)
+}
+
+
+
+type NavButtonProps = {
+	label: string,
+	path: string,
+	icon: () => React.ReactNode,
+	onClick?: () => void,
+}
+
+const NavButton: FC<NavButtonProps> = ({ label, path, icon, onClick }: NavButtonProps) => {
+	const { pathname } = useLocation();
+
+	return (
+		<Link to={path !== "/logout" ? path : "#"} className={`flex items-center py-3 pl-[17px] border-l-[3px] ${(pathname === path) ? "border-white" : "border-transparent"} font-medium transition-all text-[#808080] hover:pl-6 hover:border-white hover:text-white hover:bg-[#14131b]`}
+		onClick={onClick}>
+		<div className="w-6 h-6 mr-5">
+				{icon()}
+			</div>
+		{label}
+		{
+		/*<span className="flex items-center ml-auto mr-5 px-1 rounded text-[12px] bg-blue-600">185</span>*/
+		}
+	</Link>
+	)
+}
+
+
+
+function Sidebar() {
+
+	const { sideBarVisibility } = useSelector((state: RootState) => state.ui);
+	const dispatch = useDispatch();
+
+	const navItems = [
+		{
+			label: 'Home',
+			icon: () => <Home className="" size={iconSize} color="white" />,
+			path: "/",
+
+		},
+		{
+			label: "Video",
+			icon: () => <Video className="" size={iconSize} color="white" />,
+			path: routes.video,
+
+		},
+		{
+			label: "Logout",
+			icon: () => <LogOutFC className="" size={iconSize} onClick color="white" />,
+			path: "/logout",
+			onClick: () => dispatch(logoutUser())
+		}];
+
+	return (
+		<div style={{}} className="w-48 md:w-64 z-20 relative md:absolute bg-blackBg shadow h-full flex-col justify-between">
+			<div className="logo mt-5">
+				<img style={{
+					"objectFit": "contain"
+				}} src={logo} alt="Logo" />
+
+			</div>
+
+
+			<nav className="navItems flex flex-col">
+				{navItems.map((navItem, indx) => (
+					<NavButton key={indx} label={navItem.label} icon={navItem.icon} path={navItem.path} onClick={navItem.onClick} />
+					
+				))}
+
+			</nav>
+
+			<div className="mb-auto">
+
+			</div>
+
+		</div>
+  );
+}
+
+export default Sidebar
